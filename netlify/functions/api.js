60df84e609b04765693a84eaf69301f8a48aefca
timeenjoyed <timeenjoyed.twitch@gmail.com>
2023-05-28 23:24:03 -0700

    fix: /dist path for javascript

--- netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { Router } = require("express");
-export async function handler(event, context) {
-  const api = express();
-  const router = Router();
-  router.get('/hello', (req, res) => res.send('Hello World!'));
-  api.use('/api/', router);
-  return serverless(api)(event, context);
-}
